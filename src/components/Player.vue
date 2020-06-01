e023deb34383621d2d5d0ed02737552402e73c24
hondori <kevin479859147@gmail.com>
2020-06-01 10:38:51 +0800

    no message

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,39 +1,21 @@
 <template>
   <div class="player">
-    <div class="dics">
-      唱片
-    </div>
-    <div class="vol">
-      <van-slider v-model="value" :step="10" />
-    </div>
-    <div class="controls">
-      <van-row>
-        <van-col><van-button type="default">prev</van-button></van-col>
-        <van-col><van-button type="default">play/pause</van-button></van-col>
-        <van-col><van-button type="default">next</van-button></van-col>
-      </van-row>
-    </div>
+    <audio src="../assets/test.mp3" controls preload>您的浏览器不支持 audio 标签。</audio>
+    <div class="dics">唱片</div>
+    <div class="vol">音量</div>
+    <div class="controls">控制</div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { Col, Row, Icon, Button, Slider } from "vant";
-Vue.use(Col)
-  .use(Row)
-  .use(Icon)
-  .use(Button)
-  .use(Slider);
 export default {
   name: "Player",
   props: {
-    msg: String,
+    msg: String
   },
   data: function() {
-    return {
-      value: 50,
-    };
-  },
+    return {};
+  }
 };
 </script>
 
