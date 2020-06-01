a3f99b4364be9eaf2af4fe5ee1dc243cccb01858
hondori <kevin479859147@gmail.com>
2020-06-01 15:04:17 +0800

    master

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="player">
-    <audio src="../assets/test.mp3" controls preload>您的浏览器不支持 audio 标签。</audio>
+    <audio src="../assets/audio/test.mp3" controls preload>您的浏览器不支持 audio 标签。</audio>
     <div class="dics">唱片</div>
     <div class="vol">音量</div>
     <div class="controls">控制</div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,16 +8,7 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Main",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Main.vue"),
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "main" */ "../views/Main.vue"),
   },
 ];
 
